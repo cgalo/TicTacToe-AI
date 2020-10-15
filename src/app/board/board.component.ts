691037189ec1814515547a206f01d143c55c0775
cgalo <cgalo13@gmail.com>
2020-10-14 23:42:27 -0400

    Updated Board and Square component
    Overview:
    - Added logic to handle the input on click per square
    - Added styling to both components
    - Tested to work as expected

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -3,13 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: []
+  styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
+  board: string[][];
+  currentPlayer: string;
+  winner: string;
+  isGameDone: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame = () => {
+    this.isGameDone = false;
+    this.winner = null;
+    this.currentPlayer = 'X';
+    this.board = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ];
+  }
+
+  changePlayer = () => {
+    this.currentPlayer = (this.currentPlayer === 'X') ? 'O' : 'X';
+  }
+
+  makeMove = (row: number, col: number) => {
+    if (!this.board[row][col]){
+      this.board[row][col] = this.currentPlayer;
+      this.changePlayer();
+    }
   }
 
 }
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,10 +2,13 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  templateUrl: './square.component.html',
-  styles: []
+  template: `
+    <button> {{value}} </button>
+  `,
+  styleUrls: ['./square.component.scss']
 })
 export class SquareComponent  {
+
   @Input() value: 'X' | 'O';
 
 }
